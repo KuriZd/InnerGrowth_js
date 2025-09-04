9eac199ae190b6e596191d6cadbd0fc2f48ff508
KuriZd <KuriZd@protonmail.com>
2025-09-04 12:28:12 -0600

    [UPTADE] new layout

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -9,13 +9,13 @@ console.log(colorScheme)
 
   return (
     <View className='flex-1'>
-      <Stack screenOptions={{ headerStyle: { backgroundColor: '#ddd' }, headerTintColor: 'black' }}>
-        <Stack.Screen name='index' options={{ title: 'Home' }} />
-        <Stack.Screen name='profile' options={{ title: 'Profile' }} />
-        <Stack.Screen name='settings' options={{ title: 'Settings' }} />
+      <Stack screenOptions={{ headerStyle: { backgroundColor: '#fff' }, headerTintColor: 'black' }}>
+        <Stack.Screen name='index' options={{ title: 'Home', handers: {visible: true} }} />
+        <Stack.Screen name='profile' options={{ title: 'Profile', handers: {visible: false} }} />
+        <Stack.Screen name='settings' options={{ title: 'Settings', handers: {visible: false} }} />
         <Stack.Screen name='about' options={{ title: 'About', headers: { visible: false } }} />
       </Stack>
-      <Text className='text-2xl font-bold fixed bottom-0 left-0 right-0 text-center'>noi se que hago</Text>
+      
     </View>
   )
 }
